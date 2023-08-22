00fe77e1604250fe000c2c194dae96e36df39f26
ThomDeV-bit <120723852+ThomDeV-bit@users.noreply.github.com>
2023-08-22 13:43:53 -0400

    feat dto

--- src/product/dto/product.dto.ts
+++ src/product/dto/product.dto.ts
@@ -1,54 +1,56 @@
-// import { ApiProperty } from "@nestjs/swagger";
-// import { Expose, Type } from "class-transformer";
-// import { IsNotEmpty, IsString, IsInt, Min, IsPositive } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
+import { Expose, Type } from "class-transformer";
+import { IsNotEmpty, IsString, IsInt, Min, IsPositive } from "class-validator";
+import { CharacteristicsProductsDTO } from '../characteristics-product/dto/characteristics-products.dto';
+import { ImageProdutoDTO } from "../image-product/dto/image-product.dto";
 
 
 
-// export class CriaProdutoDTO {
+export class CriaProdutoDTO {
 
-//     @ApiProperty({ name: "nome do Produto" })
-//     @Expose({ name: "nomeProduto" })
-//     @IsNotEmpty()
-//     @IsString()
-//     nome: string;
-
-
-//     @ApiProperty({ name: "valor do Produto" })
-//     @Expose({ name: "valorProduto" })
-//     @IsInt()
-//     @IsPositive()
-//     valor: number;
+    @ApiProperty({ name: "nome do Produto" })
+    @Expose({ name: "nomeProduto" })
+    @IsNotEmpty()
+    @IsString()
+    nome: string;
 
 
-
-//     @ApiProperty({ name: "quantidade do Produto" })
-//     @Expose({ name: "quantidadeProduto" })
-//     @IsInt()
-//     @Min(1)
-//     quantidade: number;
+    @ApiProperty({ name: "valor do Produto" })
+    @Expose({ name: "valorProduto" })
+    @IsInt()
+    @IsPositive()
+    valor: number;
 
 
 
-//     @ApiProperty({ name: "descriçao do Produto" })
-//     @Expose({ name: "descriçaoProduto" })
-//     descricao: string;
+    @ApiProperty({ name: "quantidade do Produto" })
+    @Expose({ name: "quantidadeProduto" })
+    @IsInt()
+    @Min(1)
+    quantidade: number;
 
 
-//     @ApiProperty({ name: "caracteristicas do Produto" })
-//     @Expose({ name: "caracteristicasProduto" })
-//     @Type((x) => CaracteristicaProdutoDTO[])
-//     caracteristicas: CaracteristicaProdutoDTO[];
+
+    @ApiProperty({ name: "descriçao do Produto" })
+    @Expose({ name: "descriçaoProduto" })
+    descricao: string;
 
 
-//     @ApiProperty({ name: "Imagem do Produto" })
-//     @Expose({ name: "ImagemProduto" })
-//     @Type((x) => ImagemProdutoDTO[])
-//     imagens: ImagemProdutoDTO[];
+    @ApiProperty({ name: "caracteristicas do Produto" })
+    @Expose({ name: "caracteristicasProduto" })
+    @Type((x) => CharacteristicsProductsDTO)
+    caracteristicas: CharacteristicsProductsDTO[];
 
 
-//     @ApiProperty({ name: "categoria do Produto" })
-//     @Expose({ name: "categoriaProduto" })
-//     categoria: string;
+    @ApiProperty({ name: "Imagem do Produto" })
+    @Expose({ name: "ImagemProduto" })
+    @Type((x) => ImageProdutoDTO)
+    imagens: ImageProdutoDTO[];
 
 
-// }
+    @ApiProperty({ name: "categoria do Produto" })
+    @Expose({ name: "categoriaProduto" })
+    categoria: string;
+
+
+}
