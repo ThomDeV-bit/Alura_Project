61d50bc1466baf9d67c793cc97ecc3cb4c4b67a5
ThomDeV-bit <120723852+ThomDeV-bit@users.noreply.github.com>
2023-08-22 13:45:14 -0400

    feat product repository

---
+++ src/product/repository/product.repository.ts
@@ -0,0 +1,12 @@
+import { DataSource, Repository, TypedEventEmitter } from "typeorm";
+import { Injectable } from "@nestjs/common";
+import { ProductEntity } from "../../database/entites/product.entity";
+
+
+
+@Injectable()
+export class ProductRepository extends Repository <ProductEntity>{
+    constructor(public  dataSource : DataSource){
+        super(ProductEntity, dataSource.createEntityManager());
+}
+}
