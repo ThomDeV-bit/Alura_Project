a50b06f0718695d83045e7bcd4b9b2156203225b
ThomazTodimo <120723852+ThomDeV-bit@users.noreply.github.com>
2023-09-27 16:52:14 -0400

    feat edit config

--- src/config/logger/logger-config.ts
+++ src/config/logger/logger-config.ts
@@ -1,10 +1,10 @@
 import { pino } from 'pino';
 export const pinoMock = pino({
-  level: 'debug',
-  transport: {
-    target: 'pino-pretty',
-    options: {
-      colorize: true,
+    level: 'debug',
+    transport: {
+        target: 'pino-pretty',
+        options: {
+            colorize: true,
+        },
     },
-  },
 });
