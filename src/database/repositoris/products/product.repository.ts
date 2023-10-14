84fb9ec307796646deacf1f9042e0041f3aad801
ThomDeV-bit <120723852+ThomDeV-bit@users.noreply.github.com>
2023-10-14 17:57:53 -0300

    fix globlal exception

--- src/database/repositoris/products/product.repository.ts
+++ src/database/repositoris/products/product.repository.ts
@@ -1,4 +1,4 @@
-import { DataSource, Repository, TypedEventEmitter } from "typeorm";
+import { DataSource, In, Repository, TypedEventEmitter } from "typeorm";
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { IProductRepository, ProductEntity } from "src/database/entites/product.entity";
@@ -9,10 +9,15 @@ import { IProductRepository, ProductEntity } from "src/database/entites/product.
 export class ProductRepository implements IProductRepository {
     constructor(
         @InjectRepository(ProductEntity)
-        private readonly productRepository : Repository<ProductEntity>
-        ){}
+        private readonly productRepository: Repository<ProductEntity>
+    ) { }
     async getAll(): Promise<ProductEntity[]> {
         return await this.productRepository.find()
-        }
+    }
+
+    async getProductId(param: string) {
+        let list: ProductEntity[]
+        const product = await this.productRepository.findBy({ id: In(list) })
+    }
 }
 
